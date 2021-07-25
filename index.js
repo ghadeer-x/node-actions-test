bc2b08a3a18964397507261664f28f4e722d7d89
Ghadeer <g.alkhathlan@gmail.com>
2021-07-25 10:49:57 +0300

    add request logger

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 // importing express framework
 const express = require('express');
 const app = express();
+
+app.use(function (error, req, res, next) {
+  console.log('reciving request', req.path);
+  next();
+});
 // Respond with "hello world" for requests that hit our root "/"
 app.get('/', function (req, res) {
   console.log('test log');
   return res.send('Hello World xxxxx');
 });
+
 // listen to port 7000 by default
 app.listen(process.env.PORT || 7000, () => {
   console.log('Server is running');
