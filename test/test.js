6964f066b03d42db03ce0e31399f49de3dfc8bbf
Ghadeer <g.alkhathlan@gmail.com>
2021-07-25 09:52:58 +0300

    rewrite hello

--- test/test.js
+++ test/test.js
@@ -2,6 +2,6 @@ const request = require('supertest');
 const app = require('../index');
 describe('GET /', () => {
   it('respond with Hello World', (done) => {
-    request(app).get('/').expect('Hello World xxx', done);
+    request(app).get('/').expect('Hello World xxxxx', done);
   });
 });
